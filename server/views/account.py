581d4cc1a20f0572b422c58f0aeafd3ff1b5af7b
Yongseon Lee <yongseonlee0@gmail.com>
2018-02-10 16:02:26 +0900

    Remove IsAuthenticated import
    It is in DEFAULT_PERMISSION_CLASSES already

--- server/views/account.py
+++ server/views/account.py
@@ -6,7 +6,6 @@ from django.shortcuts import redirect, render
 from django.urls import reverse
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
 
 
 def logout(request):
